d9b4ea515814c738054a4709a913a95f05e1b4be
Kevin Andrews <andrewskevin92@gmail.com>
2023-05-18 22:55:23 -0700

    work on WhyChoose component

--- src/components/WhyChoose/index.tsx
+++ src/components/WhyChoose/index.tsx
@@ -1,7 +1,55 @@
-import React from 'react';
+import styles from './styles.module.scss';
 
 const WhyChoose = () => {
-  return <div>WhyChoose</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <h2 className={styles.header}>why choose masakali retreat</h2>
+      </div>
+      <div className={styles.container}>
+        <div className={styles['feature-container']}>
+          <span className={styles.header}>
+            <span className="material-symbols-outlined">pool</span>
+            <h4>private infinity pools</h4>
+          </span>
+
+          <p>
+            Each villa has its own private infinity pool overlooking the rice
+            fields. The luxurious pools are surrounded by beautiful gardens to
+            ensure your privacy while enjoying a relaxing dip where you can also
+            enjoy an amazing floating breakfast.
+          </p>
+        </div>
+        <div className={styles['feature-container']}>
+          <span className={styles.header}>
+            <span className="material-symbols-outlined">pool</span>
+            <h4>beautiful views</h4>
+          </span>
+
+          <p>
+            Cascading Balinese rice fields, majestic mountains, and a
+            spectacular view of the dense and wild jungle. If you’re lucky, you
+            may even see monkeys swinging in the trees! Make sure you experience
+            the magic of Masakali at sunset and then the fire flies that light
+            up the rice terraces at night.
+          </p>
+        </div>
+        <div className={styles['feature-container']}>
+          <span className={styles.header}>
+            <span className="material-symbols-outlined">pool</span>
+            <h4>spa services</h4>
+          </span>
+
+          <p>
+            Each villa has its own private infinity pool overlooking the rice
+            fields. The luxurious pools are surrounded by beautiful gardens to
+            ensure your privacy while enjoying a relaxing dip where you can also
+            enjoy an amazing floating breakfast.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WhyChoose;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,29 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+/* eslint-disable @next/next/no-page-custom-font */
+import { type AppType } from 'next/app';
+import { type Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
 
-import { api } from "~/utils/api";
+import { api } from '~/utils/api';
 
-import "~/styles/globals.css";
+import '~/styles/globals.css';
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional"
+        />
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 };
 
