9bc81584e84180a66ce00b6a6594fe3233c3be06
Kevin Andrews <andrewskevin92@gmail.com>
2024-10-30 14:24:59 -0700

    add bookandlink page

---
+++ src/app/booking/page.tsx
@@ -0,0 +1,11 @@
+const BookingPage = () => {
+  return (
+    <iframe
+      className="w-full h-screen"
+      src="https://bookandlink.com/booking/masakali-retreat_13831.html"
+      height="100vh"
+    />
+  );
+};
+
+export default BookingPage;
