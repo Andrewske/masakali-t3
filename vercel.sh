f48b76da9446e7b73b2e6a18b24dc20b9f47c1f1
= <andrewskevin92@gmail.com>
2025-04-10 20:34:27 -0700

    update build command

--- vercel.sh
+++ vercel.sh
@@ -1,8 +1,8 @@
 if [[ $VERCEL_ENV == "production" || $VERCEL_ENV == "preview" ]] ; then
-  npm run vercel-build
+  npm run build
   curl --proto '=https' --tlsv1.2 -LsSf https://github.com/PostHog/posthog/releases/download/posthog-cli-v0.0.4/posthog-cli-installer.sh | sh
   /vercel/.posthog/posthog-cli --host https://eu.posthog.com sourcemap inject --directory ./.next/static/chunks
   /vercel/.posthog/posthog-cli --host https://eu.posthog.com sourcemap upload --directory ./.next/static/chunks
 else
-  npm run vercel-build
+  npm run build
 fi
